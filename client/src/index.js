8028b84586af772dc7fd719b430e30cc9f689f5e
periana00 <periana00@gmail.com>
2022-07-05 21:08:23 +0900

    임시 백업

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
+import Search from "./routes/main"
+import Result from "./routes/search";
+import Chart from "./routes/search/chart";
+import News from "./routes/search/news";
+
+
 import reportWebVitals from './reportWebVitals';
+
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Search />}>
+          <Route path="search" element={<Result />}>
+            <Route path="chart" element={<Chart />} />
+            <Route path="news" element={<News />} />
+          </Route>
+        </Route>
+        {/* <Route
+          path="*"
+          element={
+            <main style={{ padding: "1rem" }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        /> */}
+      </Routes>
+    </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
